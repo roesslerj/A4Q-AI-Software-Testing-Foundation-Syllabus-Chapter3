a77b25bf4b80d70d53655bd5592c35a398ad2195
Jeremias Roessler <roessler@retest.de>
2019-09-11 20:26:45 +0200

    Add both unix and windows scripts for test compilation and execution

---
+++ consistency-oracle/compile.sh
@@ -0,0 +1,3 @@
+javac -cp "lib/*" src/main/java/RemoveElementTransformer.java -d target
+javac -cp "lib/*:target" src/main/java/BuggyRemoveElementTransformer.java -d target
+javac -cp "lib/*:target" src/test/java/RemoveElementTransformerTest.java -d target
---
+++ consistency-oracle/runTest.bat
@@ -0,0 +1,1 @@
+java -cp "lib/*:target" org.junit.runner.JUnitCore RemoveElementTransformerTest
