fd8adcb4c514abe20dc1ffe41167083e3dd6a65d
Jeremias Roessler <roessler@retest.de>
2019-09-23 16:19:32 +0200

    Add success message at the end, so user knows it worked...

--- consistency-oracle/compile.bat
+++ consistency-oracle/compile.bat
@@ -2,3 +2,4 @@ if not exist target mkdir target
 javac -cp "lib/*" src/main/java/RemoveElementTransformer.java -d target
 javac -cp "lib/*:target/*" src/main/java/BuggyRemoveElementTransformer.java -d target
 javac -cp "lib/*:target/*" src/test/java/RemoveElementTransformerTest.java -d target
+echo "Successfully compiled code and tests"
--- consistency-oracle/compile.sh
+++ consistency-oracle/compile.sh
@@ -2,3 +2,4 @@ mkdir -p target
 javac -cp "lib/*" src/main/java/RemoveElementTransformer.java -d target
 javac -cp "lib/*:target" src/main/java/BuggyRemoveElementTransformer.java -d target
 javac -cp "lib/*:target" src/test/java/RemoveElementTransformerTest.java -d target
+echo "Successfully compiled code and tests"
