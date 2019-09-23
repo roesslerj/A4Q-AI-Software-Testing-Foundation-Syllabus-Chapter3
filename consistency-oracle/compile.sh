e589c233bb4004dafe71acf9501e403a43c5beca
Jeremias Roessler <roessler@retest.de>
2019-09-23 16:19:07 +0200

    Only create dirs if they don't already exist

--- consistency-oracle/compile.sh
+++ consistency-oracle/compile.sh
@@ -1,4 +1,4 @@
-mkdir target
+mkdir -p target
 javac -cp "lib/*" src/main/java/RemoveElementTransformer.java -d target
 javac -cp "lib/*:target" src/main/java/BuggyRemoveElementTransformer.java -d target
 javac -cp "lib/*:target" src/test/java/RemoveElementTransformerTest.java -d target
