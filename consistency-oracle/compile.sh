58b60d197ba93d0a1b35c21599c34f92c1404b58
Jeremias Roessler <roessler@retest.de>
2019-09-23 16:13:03 +0200

    Create dir if it doesn't exist

--- consistency-oracle/compile.sh
+++ consistency-oracle/compile.sh
@@ -1,3 +1,4 @@
+mkdir target
 javac -cp "lib/*" src/main/java/RemoveElementTransformer.java -d target
 javac -cp "lib/*:target" src/main/java/BuggyRemoveElementTransformer.java -d target
 javac -cp "lib/*:target" src/test/java/RemoveElementTransformerTest.java -d target
